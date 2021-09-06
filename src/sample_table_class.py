f3e9cd0ee6ac96ddeebdac3db12685c42c96c235
ustato <atsuto.fps@gmail.com>
2021-09-07 03:58:41 +0900

    Impl #1

--- src/sample_table_class.py
+++ src/sample_table_class.py
@@ -1,6 +1,7 @@
 from enum import IntEnum, auto
 import functools
 import json
+import re
 import utils
 
 
@@ -39,9 +40,11 @@ class KeyValueCell(BaseCell):
         return str(self.flags) + str(self.key).rjust(self.key_size, ' ') + str(self.data_record).rjust(self.value_size, ' ')
 
 class Header:
-    def __init__(self, pointer_start=128, pointer_size=8, prev_page="prev.page", post_page="post.page", page_max_string=65536):
+    def __init__(self, pointer_start=128, pointer_size=8, item_count=0, key_size=4, prev_page="prev.page", post_page="post.page", page_max_string=65536):
         self.pointer_start = pointer_start
         self.pointer_size = pointer_size
+        self.item_count = item_count
+        self.key_size = key_size
         self.prev_page = prev_page
         self.post_page = post_page
         self.page_max_string = page_max_string
@@ -54,16 +57,20 @@ class Page:
         self.header = str(header)
         self._page_max_string = header.page_max_string
         self.cells = functools.reduce(lambda x, y: str(x)+str(y), cells[::-1])
-        _pointers = functools.reduce(lambda x, y: str(x).rjust(header.pointer_size, ' ')+str(y+int(str(x)[-header.pointer_size:])-header.page_max_string).rjust(header.pointer_size, ' '), (header.page_max_string-sum([int(i[1]) for i in c.__dict__.items() if "size" in i[0]]) for c in cells))
-        print([])
-        self.pointers = _pointers
+        self.pointers = functools.reduce(lambda x, y: str(x).rjust(header.pointer_size, ' ')+str(y+int(str(x)[-header.pointer_size:])-header.page_max_string).rjust(header.pointer_size, ' '), (header.page_max_string-sum([int(i[1]) for i in c.__dict__.items() if "size" in i[0]]) for c in cells))
 
     def __str__(self):
-        print([
-            (self.header + self.pointers + self.cells.rjust(self._page_max_string - len(self.header) - len(self.pointers), ' '))[int(s):int(s)+20]
-            for s in self.pointers.split()
-        ])
         return self.header + self.pointers + self.cells.rjust(self._page_max_string - len(self.header) - len(self.pointers), ' ')
+
+    def sort_pointer(self, key_index="id"):
+        _header = json.loads(self.header)
+        _pointer_size = int(_header["pointer_size"])
+        _item_count = int(_header["item_count"])
+        _key_size = int(_header["key_size"])
+        _pointers = [int(self.pointers[i*_pointer_size:i*_pointer_size+_pointer_size]) for i in range(_item_count)]
+        _dict = dict(zip([c for c in re.findall(r'(\d{,4}){', self.cells)], _pointers[::-1]))
+        print(_dict)
+        self.pointers = "".join((str(s[1]).rjust(_pointer_size, ' ') for s in sorted(_dict.items(), key=lambda x:x[0])))
 
 class SampleTableDefinition(BaseJsonDto):
     def __init__(self, id, name, age):
@@ -80,13 +87,15 @@ if __name__ == '__main__':
     ]
     # print(records[0])
     # print(records[1])
-    key_cells = [
+    cells = [
         KeyValueCell(flags=TableType.SAMPLE, key_size=4, value_size=len(str(data)), key=data.id, data_record=str(data))
         for data in records
     ]
-    # print([str(i) for i in key_cells])
+    # print([str(i) for i in cells])
     page = Page(
-        header=Header(pointer_start=128, pointer_size=8, prev_page="prev.page", post_page="post.page", page_max_string=1024),
-        cells=key_cells,
+        header=Header(pointer_start=130, pointer_size=8, item_count=len(cells), key_size=4, prev_page="prev.page", post_page="post.page", page_max_string=300),
+        cells=cells,
     )
+    print(page)
+    page.sort_pointer()
     print(page)
